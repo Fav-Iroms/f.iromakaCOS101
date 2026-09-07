636e1fbb5e6cd230fae33a156ed64689949b67f4
favzy <favour.iromaka@pau.edu.ng>
2026-09-07 17:22:38 +0100

    Delete week-3/practice_1 directory

--- week-3/practice_1/target/debug/practice_1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Lenovo\OneDrive\Documents\mr\ chudi's\ space\f.iromakaCOS101\week-3\practice_1\target\debug\practice_1.exe: C:\Users\Lenovo\OneDrive\Documents\mr\ chudi's\ space\f.iromakaCOS101\week-3\practice_1\src\main.rs
