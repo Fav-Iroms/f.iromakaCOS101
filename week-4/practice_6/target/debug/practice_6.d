4936bcf5e94afcc991b54e1036ac409445ecb3c6
Favour <favour.iromaka@pau.edu.ng>
2026-09-14 22:57:24 +0100

    practice_6

---
+++ week-4/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\Lenovo\OneDrive\Documents\mr\ chudi's\ space\f.iromakaCOS101\COS101-FRESH\f.iromakaCOS101\week-4\practice_6\target\debug\practice_6.exe: C:\Users\Lenovo\OneDrive\Documents\mr\ chudi's\ space\f.iromakaCOS101\COS101-FRESH\f.iromakaCOS101\week-4\practice_6\src\main.rs
